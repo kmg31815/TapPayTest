2b6cd565fbdc09055c97cf7f9620bc267559f1cf
sherry <kmg31815@gmail.com>
2024-09-17 03:28:53 +0800

    first commit

---
+++ src/main/java/tw/com/example/app/tappayTest/TappayTestApplication.java
@@ -0,0 +1,13 @@
+package tw.com.example.app.tappayTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TappayTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TappayTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tw/com/example/app/tappayTest/TappayTestApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.com.example.app.tappayTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TappayTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
